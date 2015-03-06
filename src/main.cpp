2878bc8f25b0a35c2c6d34347879a22ce7a5d980
Jethro Muller <mlljet001@myuct.ac.za>
2015-03-06 08:48:29 +0200

    Use volimage class function to handle file reading.

--- src/main.cpp
+++ src/main.cpp
@@ -3,28 +3,16 @@
 #include <fstream>
 #include <sstream>
 
+#include "volimage.h"
+
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         std::cout << "Image base name required as first argument!" << std::endl;
         return 0;
     }
-
-    std::string imageFile = "mri_raw/" + std::string(argv[1]);
-    std::string imageDat = imageFile + ".data";
-
-    std::ifstream inFile(imageDat);
-    std::string line;
-
-    int width = 0, height = 0, numImages = 0;
-    if (inFile.is_open()) {
-        while (std::getline(inFile, line)) {
-            std::istringstream ss(line);
-            ss >> width >> std::ws >> height >> std::ws >> numImages;
-        }
-    }
-    std::cout << width << " " << height << numImages << std::endl;
-    std::cout << imageDat << "\t" << imageFile << std::endl;
+    VolImage volImage;
+    volImage.readImages(std::string(argv[1]));
 
     for (int i = 0; i < argc; i++) {
         std::cout << argv[i] << std::endl;
